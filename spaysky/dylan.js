4c41e369bd3cc870d0fb15bde017084f4caa4e8d
John Zeichman <johnzeichman@Johns-MacBook-Pro.local>
2020-05-19 20:27:14 -0400

    trying to change gallery pics depending on gallery

--- spaysky/dylan.js
+++ spaysky/dylan.js
@@ -6,19 +6,25 @@ const stickPics = document.querySelectorAll(".stickPics");
 const wickerPics = document.querySelectorAll(".wickerPics");
 const stickGallery = document.querySelector(".stick-window");
 const wickerGallery = document.querySelector(".wicker-window");
+// galleries created as array to loop through and remove show when closing modal
 const galleries = document.querySelectorAll(".gallery");
 // event listeners for buttons
 
 workBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
     imageTrack.classList.add("show");
+
     let work = event.target;
+    work.classList.add("show");
+
     if (work === stickBtn) {
       stickGallery.classList.add("show");
+      stickGallery.classList.add("currentGallery");
     } else if (work === wickerBtn) {
       console.log("buns");
-      stickGallery.classList.remove("show");
+
       wickerGallery.classList.add("show");
+      wickerGallery.classList.add("currentGallery");
     }
   });
 });
@@ -31,8 +37,12 @@ let counter = 0;
 const modalClose = document.querySelector(".modal-close ");
 
 //modal buttons eventlisteners
-
+// added classlist of current gallery to use to create variable to move slides
+// instead of stickPics it needs to be a variable
 modalNext.addEventListener("click", function () {
+  if (1 > 0) {
+    console.log("burp");
+  }
   let currentSlide = stickPics[counter];
   currentSlide.classList.remove("show");
   counter++;
@@ -42,6 +52,17 @@ modalNext.addEventListener("click", function () {
   }
   stickPics[counter].classList.add("show");
 });
+// modalNext.addEventListener("click", function () {
+//   let currentSlide = stickPics[counter];
+//   currentSlide.classList.remove("show");
+//   counter++;
+
+//   if (counter > stickPics.length - 1) {
+//     counter = 0;
+//   }
+//   stickPics[counter].classList.add("show");
+// });
+
 modalPrev.addEventListener("click", function () {
   let currentSlide = stickPics[counter];
   currentSlide.classList.remove("show");
@@ -56,5 +77,6 @@ modalClose.addEventListener("click", function () {
   imageTrack.classList.remove("show");
   galleries.forEach(function (gallery) {
     gallery.classList.remove("show");
+    gallery.classList.remove("currentGallery");
   });
 });
